d44efd5aa66d6ea6a6c00e8f609cf31f28232b40
myic <myic@sina.com>
2014-10-23 22:22:08 +0800

    add comment

--- src/org/sousai/domain/Match.java
+++ src/org/sousai/domain/Match.java
@@ -9,17 +9,18 @@ import java.sql.Timestamp;
 public class Match implements Serializable{
 
 	private static final long serialVersionUID = -2347973669337577325L;
-	private Long id;
-	private String name;
+	
+	private Long id;	//数据库自添加，不用传递给后台
+	private String name;	
 	private String type;
 	private Date beginTime;
 	private Date endTime;
-	private Long courtId;
-	private String rule;
+	private Long courtId;	//可传可不传
+	private String rule;	//可传可不传
 	private Timestamp relTime;
 	private char verify;
-	private String score;
-	private Long userId;
+	private String score;	//可传可不传
+	private Long userId;	//可传可不传
 	
 	//默认构造函数
 	public Match()
