b5a87159756fda7d1278ff5d0ccf7eea81d194ab
xmoyKing <822547462@qq.com>
2014-10-30 23:51:26 +0800

    adsf

--- sqlFolder/sousai1.1/MESSAGE.sql
+++ sqlFolder/sousai1.1/MESSAGE.sql
@@ -2,12 +2,13 @@ create table MESSAGE
 (
 ID bigint not null primary key auto_increment,
 PARENTID bigint null,
+ROOTID bigint null,
 USERID int not null,
 COURTID int not null,
 TIME timestamp not null default current_timestamp,
 MESG varchar(255) not null,
 USERNAME varchar(255) not null,
-constraint foreign key(parent_ID) references MESSAGE(ID) on delete cascade on update cascade,
+constraint foreign key(parentID) references MESSAGE(ID) on delete cascade on update cascade,
 constraint foreign key(USERID) references USER(ID) on delete cascade on update cascade,
 constraint foreign key(COURTID) references COURT(ID) on delete cascade on update cascade,
 constraint foreign key(ROOTID) references MESSAGE(ID) on delete cascade on update cascade
