5abba1308af4adcc334f1d183e3a98d7758eb609
myic <myic@sina.com>
2015-01-11 18:47:48 +0800

    完成用户收藏，及查看收藏

---
+++ src/org/sousai/action/MarkMatchAction.java
@@ -0,0 +1,50 @@
+package org.sousai.action;
+
+import org.apache.struts2.ServletActionContext;
+import org.sousai.action.base.UserBaseAction;
+import org.sousai.common.Constant;
+import org.sousai.domain.UserMark;
+import org.sousai.tools.JSONUtils;
+
+import com.opensymphony.xwork2.Result;
+
+public class MarkMatchAction extends UserBaseAction{
+
+	private static final long serialVersionUID = -7967521480169848130L;
+	
+	private UserMark userMark;
+
+	/**
+	 * @return the userMark
+	 */
+	public UserMark getUserMark() {
+		return userMark;
+	}
+
+	/**
+	 * @param userMark the userMark to set
+	 */
+	public void setUserMark(UserMark userMark) {
+		this.userMark = userMark;
+	}
+
+	/**
+	 * @return the serialversionuid
+	 */
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+	
+	public String execute() throws Exception {
+		String value;
+		if(umg.markMatch(userMark)!=null){
+			value = Constant.SUCCESS;
+		}
+		else{
+			value = Constant.FAIL;
+		}
+		JSONUtils.toJson(ServletActionContext.getResponse(), value);
+		return null;
+	}
+
+}
---
+++ src/org/sousai/dao/impl/UserMarkDaoHibernate.java
@@ -0,0 +1,65 @@
+package org.sousai.dao.impl;
+
+import java.util.List;
+
+import org.hibernate.Session;
+import org.sousai.dao.UserMarkDao;
+import org.sousai.domain.UserMark;
+import org.sousai.vo.MatchBean;
+
+public class UserMarkDaoHibernate extends SqlHelper implements UserMarkDao {
+
+	@Override
+	public UserMark get(Integer id) {
+		return (UserMark)getHibernateTemplate().get(UserMark.class, id);
+	}
+
+	@Override
+	public Long save(UserMark userMark) {
+		return (Long) getHibernateTemplate().save(userMark);
+	}
+
+	@Override
+	public void update(UserMark userMark) {
+		getHibernateTemplate().update(userMark);
+	}
+
+	@Override
+	public void delete(UserMark userMark) {
+		getHibernateTemplate().delete(userMark);
+	}
+
+	@Override
+	public void delete(Integer id) {
+		getHibernateTemplate().delete(get(id));
+	}
+	
+	@Override
+	public Boolean markMatchByUserId(Integer userId, Integer matchId) {
+		Boolean value = false;
+		
+		return value;
+	};
+	
+	@Override
+	public List<MatchBean> findByMarkingUserId(Integer userId, int currentPage,
+			int rows) {
+		String hql = "select new org.sousai.vo.MatchBean(m.id,m.name,m.type,"
+				+ "m.beginTime,m.endTime,m.courtId,c.name,m.rule,m.relTime,"
+				+ "m.verify,m.score,m.userId,u.name) from Match m, Court c, User u, Usermark um "
+				+ "where m.courtId=c.id and u.id=m.userId and um.UserId=? and m.id=um.matchId";
+		// String hql = selectMatchBean;
+		Session session = getHibernateTemplate().getSessionFactory()
+				.getCurrentSession();
+		try {
+			return (List<MatchBean>) session.createQuery(hql)
+					.setInteger(0, userId).setMaxResults(rows)
+					.setFirstResult((currentPage - 1) * rows).list();
+		} catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+
+
+}
