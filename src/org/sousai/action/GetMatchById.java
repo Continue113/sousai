73ba2245640ef249ffc88c6b9c43a49abe707cef
myic <myic@sina.com>
2015-01-16 21:13:57 +0800

    完成通过id获取场地与比赛

---
+++ src/org/sousai/action/GetMatchById.java
@@ -0,0 +1,51 @@
+package org.sousai.action;
+
+import org.apache.struts2.ServletActionContext;
+import org.sousai.action.base.UserBaseAction;
+import org.sousai.common.Constant;
+import org.sousai.tools.CommonUtils;
+import org.sousai.tools.JSONUtils;
+import org.sousai.vo.MatchBean;
+
+public class GetMatchById extends UserBaseAction{
+
+	private static final long serialVersionUID = -9162366868907349819L;
+	
+	private Integer id;
+
+	/**
+	 * @return the id
+	 */
+	public Integer getId() {
+		return id;
+	}
+
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	/**
+	 * @return the serialversionuid
+	 */
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+	
+	public String execute() throws Exception{
+		MatchBean value = null;
+		if(!CommonUtils.isNullOrEmpty(id))
+		{
+			value = cmg.findMatchBeanById(id);
+			JSONUtils.toJson(ServletActionContext.getResponse(), value);
+		}
+		else{
+			JSONUtils.toJson(ServletActionContext.getResponse(), Constant.ERROR);
+		}
+		return null;
+	}
+
+	
+}
