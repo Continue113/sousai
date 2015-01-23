a2eb758df2805802316ba40464c9bae1a5071d2f
SpeedMe <leihuang.org@gmail.com>
2015-01-23 18:07:25 +0800

    是否已经登陆判断

---
+++ src/org/sousai/search/Util.java
@@ -0,0 +1,23 @@
+package org.sousai.search;
+
+import org.apache.struts2.ServletActionContext;
+import org.sousai.domain.User;
+import org.sousai.tools.JSONUtils;
+import org.sousai.vo.UserBean;
+
+import com.opensymphony.xwork2.ActionContext;
+
+public class Util {
+	public String isLogined(){
+		int userId = -1 ;
+		try {
+			User tempUser = new User((UserBean) ActionContext.getContext()
+					.getSession().get("userBean"));
+			userId = tempUser.getId();
+		} catch (Exception e) {
+			JSONUtils.toJson(ServletActionContext.getResponse(), userId);
+			System.out.println(e);
+		}
+		return null ;
+	}
+}
