97f1a2efa4b7cfcc84c6a71b344e3182fe9f2243
myic <myic@sina.com>
2015-01-26 19:50:57 +0800

    在修改比赛action中增加发布场地

--- src/org/sousai/action/UpdateMatchAction.java
+++ src/org/sousai/action/UpdateMatchAction.java
@@ -1,9 +1,9 @@
 package org.sousai.action;
 
-
 import org.apache.struts2.ServletActionContext;
 import org.sousai.action.base.UserBaseAction;
 import org.sousai.common.Constant;
+import org.sousai.domain.Court;
 import org.sousai.domain.Match;
 import org.sousai.tools.JSONUtils;
 
@@ -11,7 +11,9 @@ public class UpdateMatchAction extends UserBaseAction {
 
 	private static final long serialVersionUID = -5680488596868566421L;
 
+	private Boolean isCourt;
 	private Match match;
+	private Court court;
 
 	/**
 	 * @return the match
@@ -29,6 +31,35 @@ public class UpdateMatchAction extends UserBaseAction {
 	}
 
 	/**
+	 * @return the isCourt
+	 */
+	public Boolean getIsCourt() {
+		return isCourt;
+	}
+
+	/**
+	 * @param isCourt
+	 *            the isCourt to set
+	 */
+	public void setIsCourt(Boolean isCourt) {
+		this.isCourt = isCourt;
+	}
+
+	/**
+	 * @return the court
+	 */
+	public Court getCourt() {
+		return court;
+	}
+
+	/**
+	 * @param court the court to set
+	 */
+	public void setCourt(Court court) {
+		this.court = court;
+	}
+
+	/**
 	 * @return the serialversionuid
 	 */
 	public static long getSerialversionuid() {
@@ -38,8 +69,14 @@ public class UpdateMatchAction extends UserBaseAction {
 	public String execute() throws Exception {
 		String value;
 		try {
-			cmg.updateMatch(match);
-			value = Constant.SUCCESS;
+			if ((!isCourt || (court != null && umg.releaseCourt(court) != null))
+					// && getMatch() != null && umg.relMatch(match) != 0) {
+					&& getMatch() != null) {
+				cmg.updateMatch(match);
+				value = Constant.SUCCESS;
+			}else{
+				value = Constant.FAIL;
+			}
 		} catch (Exception e) {
 			e.printStackTrace();
 			value = Constant.ERROR;
